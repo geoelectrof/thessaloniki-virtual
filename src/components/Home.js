92b483f6b3be4eae8a90776837576b95aa335212
geoelectrof <dimitris.geo@gmail.com>
2022-07-18 15:08:38 +0300

    Uninstalled node-sass, installed sass and ran npm audit fix --force

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import { Button } from "react-bootstrap"
 export default function Home() {
     return  (
         <main>
-            <h1> Home Page! </h1>
+            <h1> Home Page!! </h1>
             <Button>Hello</Button>
             <h2>Test</h2>
         </main>
